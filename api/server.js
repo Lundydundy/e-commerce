44a52ba497a8793b8ba56983e284e4fc17423e3f
Matteo Giliotti <matteogiliotti@gmail.com>
2024-03-12 12:53:58 -0400

    check session

--- api/server.js
+++ api/server.js
@@ -58,7 +58,8 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // 30 days
         secure: false,
-        httpOnly: false
+        httpOnly: false,
+        sameSite: "none"
     }
 
 }))
