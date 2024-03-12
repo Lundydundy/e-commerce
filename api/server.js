9352ff69168600c4fd488c18e79117ec4e7ee0f3
Matteo Giliotti <matteogiliotti@gmail.com>
2024-03-12 11:07:00 -0400

    redis

--- api/server.js
+++ api/server.js
@@ -35,6 +35,11 @@ const redisClient = redis.createClient({
     legacyMode: true
 }
 )
+redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+});
+redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');})
 
 const connection = async () => {
     await redisClient.connect();
