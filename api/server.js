f69a808e72915c3c9a05ba2436ee917b178659f1
Matteo Giliotti <matteogiliotti@gmail.com>
2024-03-12 16:07:42 -0400

    cook

--- api/server.js
+++ api/server.js
@@ -66,7 +66,7 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cookieParser());
+app.use(express.cookieParser());
 
 
 app.use((req, res, next) => {
